cc658d9cef9e07c491510c75cdd575c5fc156428
devolps-survive <ethic345@gmail.com>
2025-12-26 08:14:17 -0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,23 @@
+import { useTasks } from "../store/taskContext";
+import { Link } from "react-router-dom";
 
+const Header = () => {
+  const { darkMode, setDarkMode } = useTasks();
+
+  return (
+    <header className={darkMode ? "dark" : ""}>
+      <h1>Task Tracker</h1>
+
+      <button onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/stats">Stats</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
